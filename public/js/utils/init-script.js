92ab3a173283297104833875b6a023add5e06374
Tomas Chernov <Chernatom@gmail.com>
2017-02-18 19:57:13 -0300

    Minor mobile nav re-styling. Added better nav functionality

--- public/js/utils/init-script.js
+++ public/js/utils/init-script.js
@@ -2,12 +2,43 @@ function init () {
   // Set latest works
   window.latestWorks = "sculpture";
 
-  // Nav functionality
+  // General use DOM elements
   const $mobileNav = $('#mobile-nav');
   const $hamburgerIcon = $('.hamburger-cont');
-  $hamburgerIcon.on('click', (e) => {
-    $mobileNav.toggleClass('mobile-open');
+
+  // General use helpers
+  function checkClassNames ($target, classList) {
+    return classList.some((className) => {
+      return $target.hasClass(className);
+    });
+  }
+
+  // Global click handler
+  $('html').on('click', (e) => {
+    const $target = $(e.target);
+    console.log($target);
+    navEvents($target);
+    imageEvents($target);
   });
+
+  // Nav functionality
+  function navEvents ($target) {
+    const allowedClasses = ['hamburger', 'hamburger-cont', 'hamburger-top', 'hamburger-bottom'];
+    const isNavEvent = checkClassNames($target, allowedClasses);
+    const navIsOpen = $mobileNav.hasClass('mobile-open');
+
+    if (isNavEvent) {
+      $mobileNav.toggleClass('mobile-open');
+    } else if (navIsOpen) {
+      $mobileNav.removeClass('mobile-open');
+    }
+  }
+
+  // Images Functionality
+  function imageEvents ($target) {
+    const allowedClasses = ['image-box-outer', 'image-box-img'];
+
+  }
 }
 
 module.exports = init;
