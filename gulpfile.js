0f34ee474abd17ba248c6c9c4e5ee322272454b4
cherna <chernatom@gmail.com>
2018-10-22 15:43:13 -0300

    update project

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
 const stylus = require('gulp-stylus');
 const notify = require('gulp-notify');
 const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const cache = require('gulp-cache');
 const pug = require('gulp-pug');
 const plumber = require('gulp-plumber');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 
 const browserify = require('browserify');
+const babelify = require('babelify');
 
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 
 // Stylus compilation
 
-gulp.task('styles', function() {
+gulp.task('styles', function (done) {
   gulp.src('public/styl/style.styl')
     .pipe(plumber())
     .pipe(stylus({
@@ -30,41 +30,47 @@ gulp.task('styles', function() {
     .pipe(cleanCSS({ level: 2 }))
     .pipe(gulp.dest('./css/'))
     .pipe(notify({ message: 'Styles task complete' }));
+  done();
 });
 
 // Image Compression
 
-gulp.task('images', function() {
-  return gulp.src('public/images/*')
+gulp.task('images', function(done) {
+  gulp.src('public/images/*')
     .pipe(plumber())
     .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
     .pipe(gulp.dest('./img'))
     .pipe(notify({ message: 'Images task complete' }));
+  done();
 });
 
 // Browserify
 
-gulp.task('browserify', function() {
-  return browserify('public/js/index.js')
-    .transform('babelify', {
-      presets: ["es2015"],
-      ignore: /wow\.min\.js/
-    })
-    .bundle()
-    .on('error', (err) => console.error( new Error(err) ))
-    //Pass desired output filename to vinyl-source-stream
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    // .pipe(uglify())
-    // Start piping stream to tasks!
-    .pipe(gulp.dest('./js'))
-    .pipe(notify({message: 'Browserify task complete'}))
+gulp.task('browserify', function(done) {
+  // create the Browserify instance.
+  browserify({
+    entries: ['public/js/index.js'],
+    debug: true,
+    transform: [
+      babelify.configure({
+        presets: ["es2015"],
+        ignore: /wow\.min\.js/
+      })
+    ]
+  }).bundle()
+      .on('error', console.error)
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+      .pipe(gulp.dest('./js/'))
+
+  done();
 });
 
 // pug.compileFile('public/js/views/base/layout.pug')
 
 // Static index
-gulp.task('index', function() {
+gulp.task('index', function(done) {
   gulp.src('public/js/views/base/layout.pug')
     .pipe(plumber())
     .pipe(pug({
@@ -72,28 +78,33 @@ gulp.task('index', function() {
     }))
     .pipe(rename('index.html'))
     .pipe(gulp.dest('./'));
+  done();
 });
 
 // Default Task
 
-gulp.task('default', function() {
-    gulp.start(
-      'styles',
-      'index',
-      'browserify',
-      'images',
-      'watch');
+gulp.task('default', function(done) {
+  
+  done();
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function(done) {
 
   // Watch .scss files
-  gulp.watch(['public/**/*.styl', ], ['styles']);
+  gulp.watch(['public/**/*.styl', ], gulp.series('styles'));
 
   // Watch .js files
-  gulp.watch(['public/js/**/*.js', 'public/**/*.pug', 'views/**/*.pug'], ['index', 'browserify']);
+  gulp.watch(['public/js/**/*.js', 'public/**/*.pug', 'views/**/*.pug'], gulp.series('index', 'browserify'));
 
   // Watch image files
-  gulp.watch('public/images/*', ['images']);
+  gulp.watch('public/images/*', gulp.series('images'));
 
+  done();
 });
+
+exports.default = gulp.series(
+  'styles',
+  'index',
+  'browserify',
+  'images',
+  'watch');
